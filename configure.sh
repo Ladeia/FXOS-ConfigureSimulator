2467609dba6e3d7b96dde4ddec0f3b1dfc163e60
tecnowancer <tecnowancer@gmail.com>
2014-03-14 16:13:22 -0300

    first commit

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+IDVENDOR=`lsusb | awk '/Qualcomm/ {print $6}' | awk -F":" '{print $1}'`
+IDPROD=`lsusb | awk '/Qualcomm/ {print $6}' | awk -F":" '{print $2}'`
+
+echo SUBSYSTEM=="usb", ATTR{idVendor}=="$IDVENDOR", ATTR{idProduct}=="$IDPROD", MODE="0666" > /etc/udev/rules.d/51-android.rules
