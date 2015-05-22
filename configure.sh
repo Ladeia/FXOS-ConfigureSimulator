6d62dd602ba9479298a7415c6afde1714d6c26a7
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2015-05-22 07:00:05 -0300

    Inserindo novas regras para o keon

--- configure.sh
+++ configure.sh
@@ -4,3 +4,7 @@ IDVENDOR=`lsusb | awk '/Qualcomm/ {print $6}' | awk -F":" '{print $1}'`
 IDPROD=`lsusb | awk '/Qualcomm/ {print $6}' | awk -F":" '{print $2}'`
 
 echo SUBSYSTEM=="usb", ATTR{idVendor}=="$IDVENDOR", ATTR{idProduct}=="$IDPROD", MODE="0666", GROUP="plugdev" > /etc/udev/rules.d/51-android.rules
+echo SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="d00d", MODE="0666", GROUP="plugdev" > /etc/udev/rules.d/51-android.rules
+echo SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", ATTR{idProduct}=="$IDPROD", MODE="0666", GROUP="plugdev" > /etc/udev/rules.d/51-android.rules
+
+service udev restart
